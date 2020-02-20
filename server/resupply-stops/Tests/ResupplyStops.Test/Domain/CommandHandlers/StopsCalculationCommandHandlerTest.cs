a85522aa1d48767aac7b01f5995e1d2b59beedc7
Deividi Paganini <deividi.paganini@actminds.com>
2020-02-19 22:13:01 -0300

    add stopscalculation command handler test

---
+++ server/resupply-stops/Tests/ResupplyStops.Test/Domain/CommandHandlers/StopsCalculationCommandHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace ResupplyStops.Test.Domain.CommandHandlers
+{
+    public class StopsCalculationCommandHandlerTest
+    {
+        readonly IStopsCalculationCommandHandler subject;
+
+        public StopsCalculationCommandHandlerTest()
+        {
+            subject = new StopsCalculationCommandHandler();
+        }
+
+        [Fact]
+        public void Calculate_Should_Throw_ArgumentnullExpection_When_Distance_Is_Null()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(subject.Calculate(null))
+        }
+    }
+}

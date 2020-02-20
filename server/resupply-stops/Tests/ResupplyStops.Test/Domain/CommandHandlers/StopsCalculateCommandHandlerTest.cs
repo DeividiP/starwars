4f3ea4e824a9142756c2a7b6191dffdc3b7bb4be
Deividi Paganini <deividi.paganini@actminds.com>
2020-02-19 22:59:59 -0300

    create UT to validade the Y-wing example scenario

--- server/resupply-stops/Tests/ResupplyStops.Test/Domain/CommandHandlers/StopsCalculateCommandHandlerTest.cs
+++ server/resupply-stops/Tests/ResupplyStops.Test/Domain/CommandHandlers/StopsCalculateCommandHandlerTest.cs
@@ -1,4 +1,6 @@
-﻿using ResupplyStops.Application.Domain.CommandHandlers;
+﻿using Moq;
+using ResupplyStops.Application.Domain.CommandHandlers;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,10 +9,12 @@ namespace ResupplyStops.Test.Domain.CommandHandlers
     public class StopsCalculateCommandHandlerTest
     {
         readonly IStopsCalculateCommandHandler subject;
+        readonly IWSAPIProxy wsAPIProxyMock;
 
         public StopsCalculateCommandHandlerTest()
         {
-            subject = new StopsCalculateCommandHandler();
+            wsAPIProxyMock = new Mock<IWSAPIProxy>(); 
+            subject = new StopsCalculateCommandHandler(wsAPIProxyMock.Object);
         }
 
         [Fact]
@@ -21,6 +25,27 @@ namespace ResupplyStops.Test.Domain.CommandHandlers
             var result = await subject.Handle(distance);
 
             Assert.Equal(0, result);
+        }
+
+        [Theory]
+        [InlineData("Y-wing", 80, "1 week", 74)]
+        public async Task Calculate_Should_Return_Properly_Stops_Based_On_Ships_Data_When_Distance_Is_1000000(string shipName, int mgltPerHour, string consumables, int expectedStops)
+        {
+            int distance = 1000000;
+
+            var ywing = new StarShip
+            {
+
+                Name = shipName,
+                MGLT = mgltPerHour,
+                consumables = consumables
+            };
+
+            wsAPIProxyMock.Setup(_ => _.GetAllStarShips()).RetunrAysnc(new List<StarShip>() { ywing });
+
+            var result = await subject.Handle(distance);
+
+            Assert.Equal(expectedStops, result);
         }
     }
 }

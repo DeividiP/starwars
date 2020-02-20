b6da823e0b36c1270afb747a42e2d9f6fb3c4d4b
Deividi Paganini <deividi.paganini@actminds.com>
2020-02-20 03:31:06 -0300

    create UT to validade the 'Rebel Trasport' scenario -> it was passed

--- server/resupply-stops/Tests/ResupplyStops.Test/Domain/CommandHandlers/ShipStopsCalculateCommandHandlerTest.cs
+++ server/resupply-stops/Tests/ResupplyStops.Test/Domain/CommandHandlers/ShipStopsCalculateCommandHandlerTest.cs
@@ -22,6 +22,7 @@ namespace ResupplyStops.Test.Domain.CommandHandlers
         [Theory]
         [InlineData("Y-wing", 80, "1 week", 74)]
         [InlineData("Millennium Falcon", 75, "2 months", 9)]
+        [InlineData("Rebel Transport", 20, "6 months", 11)]
         public async Task Calculate_Should_Return_Properly_Stops_Based_On_Ships_Data_When_Distance_Is_1000000(string shipName, int mgltPerHour, string consumables, int expectedStops)
         {
             int distance = 1000000;

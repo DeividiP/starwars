7b9a5f1851d9ef933b4d9b844ef0103cce349822
Deividi Paganini <deividi.paganini@actminds.com>
2020-02-23 11:13:58 -0300

    implement controller

---
+++ server/resupply-stops/ResupplyStops/Controllers/ResupplyStopCalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResupplyStops.Application.Application.Interfaces;
+
+namespace ResupplyStops.Controllers
+{
+    [Route("api/resupply-stops")]
+    [ApiController]
+    public class ResupplyStopCalculatorController : ControllerBase
+    {
+        private readonly IResupllyStopCalculatorService _resupllyStopCalculatorService;
+
+        public ResupplyStopCalculatorController(
+            IResupllyStopCalculatorService resupllyStopCalculatorService)
+        {
+            _resupllyStopCalculatorService = resupllyStopCalculatorService;
+        }
+
+        public async Task<IActionResult> CalculateAllStarShipsResupplyStopsAsync(int distance)
+        {
+            var resupllyStops = await _resupllyStopCalculatorService.CalculateAsync(distance);
+
+            return Ok(resupllyStops);
+        }
+    }
+}

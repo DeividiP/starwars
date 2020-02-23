4856a0e6625c69e63fb367687a943de884a0d8cc
Deividi Paganini <deividi.paganini@actminds.com>
2020-02-23 11:09:33 -0300

    add first UT to ResupplyStopCalculatorController

---
+++ server/resupply-stops/Tests/ResupplyStops.Test/Controllers/ResupplyCalculatorTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ResupplyStops.Application.Application.Interfaces;
+using ResupplyStops.Application.Application.ViewModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ResupplyStops.Test.Controllers
+{
+    public class ResupplyCalculatorTest
+    {
+        private readonly ResupplyStopCalculatorController _subject;
+        private readonly Mock<IResupllyStopCalculatorService> _resupllyStopCalculatorServiceMock;
+
+        public ResupplyCalculatorTest()
+        {
+            _resupllyStopCalculatorServiceMock = new Mock<IResupllyStopCalculatorService>();
+            _subject = new ResupplyStopCalculatorController(_resupllyStopCalculatorServiceMock.Object);
+        }
+
+        public async Task Calculate_Should_Call_CalculateAsync_From_ResupplyStopCalculatorService()
+        {
+            var dummyDistance = 8;
+            _resupllyStopCalculatorServiceMock
+                  .Setup(s => s.CalculateAsync(dummyDistance))
+                  .ReturnsAsync(new List<StarShipResupplyStops>());
+
+            var response = await _subject.CalculateAllStarShipsResupplyStopsAsync(dummyDistance);
+
+            _resupllyStopCalculatorServiceMock
+                .Verify(s => s.CalculateAsync(dummyDistance), Times.Once);
+            Assert.Equal(StatusCodes.Status200OK, ((ObjectResult)response).StatusCode);
+            Assert.IsType<List<StarShipResupplyStops>>(((ObjectResult)response).Value);
+        }
+    }
+}
